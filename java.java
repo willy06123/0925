6a1b4bb27136b415b0c8bcdc8129d3ef65af9666
willy06123 <106022033@live.asia.edu.tw>
2017-09-25 15:03:00 +0800

    20172925

--- java.java
+++ java.java
@@ -1,7 +1,12 @@
 public class java {
 
-    public static void main(String[}args)
+    public static void main(String[] args){
+        System.out.println("第一個java程式");
+    }
+}
 
-        System.out.println('第一個java程式')"}
 
-        }
+
+
+
+
